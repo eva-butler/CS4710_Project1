fac918bc9c030ea6a7d9314566ab325babab850c
Sarah Kota <saaraah405@gmail.com>
2025-09-10 11:51:35 -0400

    implemented a-star search (q4), very similar to uniform cost search

--- search.py
+++ search.py
@@ -193,6 +193,36 @@ def nullHeuristic(state, problem=None):
 def aStarSearch(problem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
     "*** YOUR CODE HERE ***"
+
+    # very similar to uniform cost search 
+    if problem.isGoalState(problem.getStartState()):
+        return []
+    
+    fringe = util.PriorityQueue()
+    start_cost = 0
+    fringe.push((problem.getStartState(), [], start_cost), start_cost + heuristic(problem.getStartState(), problem))
+    visited = set()
+    best_cost = {problem.getStartState(): start_cost}
+
+    while fringe:
+        state, path, cost = fringe.pop()
+
+        if state in visited:
+            continue 
+        visited.add(state)
+
+        if problem.isGoalState(state):
+            return path 
+        
+        for next_state, next_action, next_cost in problem.getSuccessors(state):
+            new_cost = cost + next_cost
+            if (next_state not in visited) and (next_state not in best_cost or new_cost < best_cost[next_state]):
+                best_cost[next_state] = new_cost
+                priority_score = new_cost + heuristic(next_state, problem)
+                fringe.push((next_state, path + [next_action], new_cost), priority_score)
+    
+    return []
+
     util.raiseNotDefined()
 
 
